e4d69f09d4c75bd3942132c0c77f39fb73e70735
dilumhapugoda <dilumhapugoda@gmail.com>
2025-04-13 15:48:24 +0530

    navbar finished

--- app/src/main/java/com/example/trakcoin/activities/MainActivity.kt
+++ app/src/main/java/com/example/trakcoin/activities/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.trakcoin.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNav.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/trakcoin/fragments/TransactionFragment.kt
@@ -0,0 +1,19 @@
+package com.example.trakcoin.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trakcoin.R
+
+class TransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_transaction, container, false)
+    }
+}
