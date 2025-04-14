b68a6244c84cd93134aa176b6e7fc1fe071a4b6f
dilumhapugoda <dilumhapugoda@gmail.com>
2025-04-14 22:13:43 +0530

    transaction ft uis

--- app/src/main/java/com/example/trakcoin/fragments/TransactionFragment.kt
+++ app/src/main/java/com/example/trakcoin/fragments/TransactionFragment.kt
@@ -1,11 +1,14 @@
 package com.example.trakcoin.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.trakcoin.R
+import com.example.trakcoin.activities.addTransactionActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TransactionFragment : Fragment() {
 
@@ -14,6 +17,14 @@ class TransactionFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_transaction, container, false)
+        val view = inflater.inflate(R.layout.fragment_transaction, container, false)
+
+        val fabAdd = view.findViewById<FloatingActionButton>(R.id.fabAdd)
+        fabAdd.setOnClickListener {
+            val intent = Intent(activity, addTransactionActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
