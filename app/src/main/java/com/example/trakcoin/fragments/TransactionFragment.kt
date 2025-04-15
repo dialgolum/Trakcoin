3abdf3b8e6dbd7bfe80c70e6168399d7fadf053d
dilumhapugoda <dilumhapugoda@gmail.com>
2025-04-15 13:47:03 +0530

    delete transaction background color

--- app/src/main/java/com/example/trakcoin/fragments/TransactionFragment.kt
+++ app/src/main/java/com/example/trakcoin/fragments/TransactionFragment.kt
@@ -1,12 +1,16 @@
 package com.example.trakcoin.fragments
 
 import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -85,6 +89,47 @@ class TransactionFragment : Fragment() {
                     .show()
 
             }
+
+            override fun onChildDraw(
+                c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean
+            ) {
+                val itemView = viewHolder.itemView
+                val icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_delete)!!
+                val iconMargin = (itemView.height - icon.intrinsicHeight) / 2
+                val iconTop = itemView.top + iconMargin
+                val iconBottom = iconTop + icon.intrinsicHeight
+
+                val background = ColorDrawable(Color.parseColor("#EF9A9A")) //Red
+
+                if (dX < 0) { // Swiping left
+                    val iconLeft = itemView.right - icon.intrinsicWidth - iconMargin
+                    val iconRight = itemView.right - iconMargin
+                    icon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+
+                    background.setBounds(
+                        itemView.right + dX.toInt(), itemView.top,
+                        itemView.right, itemView.bottom
+                    )
+                } else {
+                    background.setBounds(0, 0, 0, 0)
+                    icon.setBounds(0, 0, 0, 0)
+                }
+
+                background.draw(c)
+                icon.draw(c)
+
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+            }
+
         })
 
         itemTouchHelper.attachToRecyclerView(recyclerView)
